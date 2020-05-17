7065ce33e22d834635acb14d08c285c2ec6fc6b5
Sanne Peters <sanne@sanstream.nl>
2020-05-17 16:45:29 +0200

    Added the PageHeader component
    integrated the 8 column grid layout

---
+++ src/components/patterns/PageHeader.vue
@@ -0,0 +1,51 @@
+<template>
+  <header class="PageHeader sanstream-grid-layout-full-viewport">
+    <TopDecoration/>
+    <nav class="sanstream-grid-layout">
+      <g-link to="/">
+        <g-image src="/logos/logo-emblem.svg" alt="sanstream logo" />
+      </g-link>
+    </nav>
+  </header>
+</template>
+
+<script>
+import TopDecoration from '~/components/patterns/TopDecoration';
+
+export default {
+  name: 'PageHeader',
+  components: {
+    TopDecoration,
+  },
+}
+</script>
+
+<style>
+.PageHeader {
+  position: relative;
+  position: sticky;
+  top: 0;
+  left: 0;
+  height: calc(7 * var(--base-size));
+  background-color: var(--colour-background);
+  width: 100vw;
+}
+
+.PageHeader .TopDecoration {
+  z-index: 1;
+  position: absolute;
+}
+
+.PageHeader nav {
+  position: relative;
+  z-index: 2;
+  position: relative;
+  padding: calc(3 * var(--base-size)) var(--base-size) 0 var(--base-size);
+  border-bottom: 1px solid var(--toned-down-border-colour);
+}
+
+.PageHeader nav.sanstream-grid-layout a {
+  grid-area: 1 / 1 / 1 / 9;
+  place-self: center;
+}
+</style>
--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,8 +1,6 @@
 <template>
-  <div class="layout">
-    <header>
-      <TopDecoration/>
-    </header>
+  <div class="default-layout">
+    <PageHeader />
     <main>
       <slot/>
     </main>
@@ -10,11 +8,11 @@
 </template>
 
 <script>
-import TopDecoration from '../components/patterns/TopDecoration';
+import PageHeader from '../components/patterns/PageHeader';
 
 export default {
   components: {
-    TopDecoration,
+    PageHeader,
   },
 }
 </script>
@@ -32,26 +30,40 @@ query {
   --colour-background: #ffffff;
   --sanstream-orange: #FF5C00;
   --sanstream-yellow: #FE9800;
-  --text-colour: #4D4D4D;
+  --text-colour:relative #4D4D4D;
   --contrasting-border-colour: #4D4D4D;
   --toned-down-border-colour: #ABABAB;
   --light-grey-fill:#E3E3E3;
   --middle-grey-fill:#C4C4C4;
   --dark-grey-fill:#878787;
+
+  --base-size: 20px;
 }
 
 body {
   margin:0;
   padding:0;
   line-height: 1.5;
+  background-color: var(--colour-background);
 }
 
-body .layout--top-deco-triangles {
-  position: absolute;
-  position:sticky;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 70px;
+.sanstream-grid-layout {
+  margin: auto;
+  display: grid;
+  grid-template-rows: auto;
+  grid-template-columns: repeat(8, calc(4 * var(--base-size)));
+  row-gap: calc(2 * var(--base-size));
+  column-gap: calc(2 * var(--base-size));
 }
+
+.sanstream-grid-layout-full-viewport {
+  display: flex;
+  flex-direction: column;
+}
+
+.sanstream-grid-layout-full-viewport > .sanstream-grid-layout {
+  flex: 1 0 auto;
+  align-self: center;
+}
+
 </style>
