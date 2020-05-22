d89fe944785dad457c9db31687fd093ae73e92bd
Sanne Peters <sanne@sanstream.nl>
2020-05-22 15:40:30 +0200

    Added a sequence line to TextAndPictureSequence

--- src/components/patterns/TextAndPictureSequence.vue
+++ src/components/patterns/TextAndPictureSequence.vue
@@ -2,11 +2,21 @@
   <article
     class="TextAndPictureSequence sanstream-grid-layout"
   >
+    <svg class="TextAndPictureSequence--line" width="3" height="2000" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <line x1="2" y1="0" x2="2" y2="2000" stroke="#ABABAB" stroke-linecap="round" stroke-dasharray="6 6"/>
+    </svg>
     <section
       class="TextAndPictureSequence--item"
       v-for="item in sequence"
       :key="item._key"
     >
+      <svg
+        width="13"
+        height="13"
+        class="TextAndPictureSequence--item-connector"
+      >
+        <circle cx="5.5" cy="5.5" r="5.5" fill="white" stroke="#ABABAB"/>
+      </svg>
       <div
         class="TextAndPictureSequence--item-text sanstream-intro-text"
       >
@@ -42,6 +52,42 @@ figure {
   margin: 0;
 }
 
+.TextAndPictureSequence {
+  position: relative;
+}
+
+.TextAndPictureSequence .TextAndPictureSequence--line {
+  position: absolute;
+  height: 100%;
+  width: 3px;
+  left: calc(50% - 3px);
+  top: 0;
+}
+
+.TextAndPictureSequence .TextAndPictureSequence--item:first-of-type::before {
+  content: '';
+  display: block;
+  position: absolute;
+  left: calc(50% - 3px);
+  top: 0;
+  height: calc(50% - 7.5px);
+  width: 3px;
+  z-index: 1;
+  background: var(--colour-background);
+}
+
+.TextAndPictureSequence .TextAndPictureSequence--item:last-of-type::after {
+  content: '';
+  display: block;
+  position: absolute;
+  left: calc(50% - 3px);
+  bottom: 0;
+  height: calc(50%);
+  width: 3px;
+  z-index: 1;
+  background: var(--colour-background);
+}
+
 .TextAndPictureSequence .TextAndPictureSequence--item {
   grid-column: 2 / 8;
   place-self: center;
@@ -49,26 +95,39 @@ figure {
   grid-template-areas: "left right";
   flex-direction: row;
   gap: calc(4 * var(--base-size));
+  position: relative;
 }
 
-.TextAndPictureSequence .TextAndPictureSequence--item .TextAndPictureSequence--item-text {
+.TextAndPictureSequence .TextAndPictureSequence--item .TextAndPictureSequence--item-figure {
+  width: calc(15 * var(--base-size));
+}
+
+.TextAndPictureSequence .TextAndPictureSequence--item:nth-child(even) .TextAndPictureSequence--item-text {
   grid-area: left;
+  text-align: right;
 }
 
 .TextAndPictureSequence .TextAndPictureSequence--item .TextAndPictureSequence--item-text > *:first-child {
   margin-top: 0;
 }
 
-.TextAndPictureSequence .TextAndPictureSequence--item .TextAndPictureSequence--item-figure {
-  grid-area: right;
-}
-
-.TextAndPictureSequence .TextAndPictureSequence--item:nth-child(even) .TextAndPictureSequence--item-text {
-  grid-area: right;
-}
-
 .TextAndPictureSequence .TextAndPictureSequence--item:nth-child(even) .TextAndPictureSequence--item-figure {
+  grid-area: right;
+}
+
+.TextAndPictureSequence .TextAndPictureSequence--item:nth-child(odd) .TextAndPictureSequence--item-text {
+  grid-area: right;
+}
+
+.TextAndPictureSequence .TextAndPictureSequence--item:nth-child(odd) .TextAndPictureSequence--item-figure {
   grid-area: left;
+}
+
+.TextAndPictureSequence .TextAndPictureSequence--item .TextAndPictureSequence--item-connector {
+  position: absolute;
+  left: calc(50% - 7.5px);
+  top: calc(50% - 7.5px);
+  z-index: 2;
 }
 
 </style>
