a72b0d264599f42ff7c28d1a5055652726f88b4c
Sanne Peters <sanstream@pop-os.localdomain>
2020-10-24 14:57:55 +0200

    Fixed syntax highlighting code examples
    https://www.notion.so/sanstream/Syntax-highlighting-in-posts-does-not-work-7d9ffbd6eea6484cabef2988c74d4fb0

--- src/components/elements/CodeExample.vue
+++ src/components/elements/CodeExample.vue
@@ -1,16 +1,46 @@
 <template>
   <!--Everything inlined so the example code is rendered withou weird indentations -->
-  <pre :class="`language-${language}`"><code>{{code}}</code></pre>
+  <pre :class="`language-${language}`">
+    <code class="sanstream-special-text">
+      <span v-html="convertedCode" />
+    </code>
+  </pre>
 </template>
 
 <script>
+import Prism from 'prismjs'
 import VueTypes from 'vue-types'
+Prism.highlightAll()
+
+/**
+ * Displays code examples for 'html', 'javascript' and 'css' using
+ * [prism.js](https://prismjs.com/).
+ */
 export default {
   name: 'CodeExample',
 
   props: {
+    /** the code to display */
     code: VueTypes.string.required,
-    language: VueTypes.string,
+    /** What type of code it is */
+    language: VueTypes.oneOf([
+      'javascript',
+      'js',
+      'css',
+      'html',
+    ]),
+  },
+
+  computed: {
+    convertedCode () {
+      return Prism.highlight(this.code, Prism.languages[this.language], this.language)
+    },
   },
 }
 </script>
+
+<style scoped>
+code {
+  font-size: 100%;
+}
+</style>
