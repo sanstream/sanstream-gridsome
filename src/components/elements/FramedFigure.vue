96b87f16d166256829768d8ef35cc6c32e7db507
Sanne Peters <sanstream@pop-os.localdomain>
2021-04-30 13:21:56 +0200

    Created fancy flowers

--- src/components/elements/FramedFigure.vue
+++ src/components/elements/FramedFigure.vue
@@ -1,8 +1,16 @@
 <template>
 <figure>
-  <div>
-    <slot />
+  <div class="frame">
+    <div class="content">
+      <slot />
+    </div>
   </div>
+  <figcaption
+    class="sanstream-body-text"
+    v-if="caption"
+  >
+    {{caption}}
+  </figcaption>
 </figure>
 </template>
 
@@ -11,22 +19,32 @@ import VueTypes from 'vue-types'
 
 export default {
   name: 'FramedFigure',
+
+  props: {
+    caption: VueTypes.string,
+  },
 }
 </script>
 
 <style scoped>
 figure {
-  border: 1px solid var(--toned-down-border-colour);
   margin: 1em 0;
 }
 
-div {
+figure > figcaption {
+  margin: 0.5em 0 1.5em 0;
+}
+.frame {
+  border: 1px solid var(--toned-down-border-colour);
+}
+
+.content {
   border: calc(0.5 * var(--base-size) - 1px) solid var(--colour-background);
   overflow: hidden;
   max-height: 100%;
 }
 
-figure > div > * {
+figure .content > * {
   display: block;
 }
 </style>
